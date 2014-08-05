500710e758087603d561bf3d4042fb41ebbc9a46
raphael <raphael@raphael-PC>
2014-08-05 19:37:14 -0300

    Criação da página de login administrador.

---
+++ ger/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Entrar - Administração</title>
+        <link rel="stylesheet" href="../libs/bootstrap/css/bootstrap.min.css">
+    </head>
+    <body role="document">
+        
+    </body>
+    <script type="text/javascript" src="../libs/jquery-1.11.1.min.js" ></script>
+    <script type="text/javascript" src="../libs/bootstrap/js/bootstrap.min.js"></script>
+</html>
