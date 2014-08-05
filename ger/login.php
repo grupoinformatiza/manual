7d37e42509765b057da1c799434bf4f9cf20f3a7
Ana Paula <Ana Paula@200.145.43.219>
2014-08-05 20:28:08 -0300

    Alterações na página de Login

--- ger/login.php
+++ ger/login.php
@@ -5,9 +5,30 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Entrar - Administração</title>
         <link rel="stylesheet" href="../libs/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="login.css">
     </head>
     <body role="document">
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <h1>Entrar</h1>
+                    <form>
+                        <div class="form-group">
+                            <label class="sr-only">Login</label>
+                            <input type="text" class="form-control input-lg" placeholder="Login"/>
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only">Senha</label>
+                            <input type="password" class="form-control input-lg" placeholder="Senha"/>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-lg btn-info pull-right">Entrar</button>        
+                        </div>
+                    </form>
+                    <div class="alert alert-danger">Dados inválidos</div>
+                </div>
+            <div/>
+        </div>
     </body>
     <script type="text/javascript" src="../libs/jquery-1.11.1.min.js" ></script>
     <script type="text/javascript" src="../libs/bootstrap/js/bootstrap.min.js"></script>
