3eb634230d0f6e12040d0445c0cd8e6d31649fac
raphael <raphael@raphael-PC>
2014-08-07 21:42:39 -0300

    Inicio do formulario da página de manutenção de usuarios.

--- ger/manut_usuario.php
+++ ger/manut_usuario.php
@@ -5,12 +5,53 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Cadastrar Usuários</title>
         <link rel="stylesheet" href="../libs/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="layout/default.css">
     </head>
     <body role="document">
         <?php require_once 'layout/cabecalho.php';?>
-        <div class="page-header">
-            <h1>Usuários</h1>
+        
+        <div class="container">
+        
+            <div class="page-header">
+                <h1>Usuários</h1>
+            </div>
+            
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    
+                    <form name="frmManutUsuario" id="frmManutUsuario" method="post" action="manut_usuario.php" class="form">
+                        
+                        <div class="form-group">
+                            <label for="txtNome">Nome</label>
+                            <input type="text" name="txtNome" id="txtNome" class="form-control input-md" />
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="txtDtNasc">Data de Nascimento</label>
+                            <input type="date" name="txtDtNasc" id="txtDtNasc" class="form-control input-md"/>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="cmbSexo">Sexo</label>
+                            <select class="form-control input-md" id="cmbSexo" name="cmbSexo">
+                                <option value="M">Masculino</option>
+                                <option value="F">Feminino</option>
+                            </select>
+                        </div>
+                        
+                 
+                        
+                        
+                    </form>                    
+                    
+                    
+                </div>                
+            </div>
+            
         </div>
+        
+        
+        
     </body>
     <script type="text/javascript" src="../libs/jquery-1.11.1.min.js" ></script>
     <script type="text/javascript" src="../libs/bootstrap/js/bootstrap.min.js"></script>
