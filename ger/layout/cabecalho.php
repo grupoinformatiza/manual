b66e107bc50bb01d74b84e9d9690c220c050baab
raphael <raphael@raphael-PC>
2014-08-07 23:58:38 -0300

    Finalização do formulario para cadastro de usuarios e criação dos links na barra de navegação

--- ger/layout/cabecalho.php
+++ ger/layout/cabecalho.php
@@ -1,22 +1,22 @@
- <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-              <a class="navbar-brand" href="index.php"><img src="../imagens/informatiza_pequeno.png">Informatiza</a>
-          </div>
-          <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">Usuário</a></li>
-              <li><a href="#contact">Tutorial</a></li>
-              <li><a href="#contact">Tópico</a></li>
-              <li><a href="#contact">Estatística</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-     </div>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Abrir navegação</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+          <a class="navbar-brand" href="index.php"><img src="../imagens/informatiza_pequeno.png"> Informatiza</a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="manut_usuario.php">Usuário</a></li>
+          <li><a href="manut_tutorial.php">Tutorial</a></li>
+          <li><a href="manut_topico.php">Tópico</a></li>
+          <li><a href="rel_estatistica.php">Estatística</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+</div>
--- ger/manut_usuario.php
+++ ger/manut_usuario.php
@@ -16,41 +16,77 @@
                 <h1>Usuários</h1>
             </div>
             
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    
-                    <form name="frmManutUsuario" id="frmManutUsuario" method="post" action="manut_usuario.php" class="form">
-                        
-                        <div class="form-group">
-                            <label for="txtNome">Nome</label>
-                            <input type="text" name="txtNome" id="txtNome" class="form-control input-md" />
-                        </div>
-                        
-                        <div class="form-group">
-                            <label for="txtDtNasc">Data de Nascimento</label>
-                            <input type="date" name="txtDtNasc" id="txtDtNasc" class="form-control input-md"/>
-                        </div>
-                        
-                        <div class="form-group">
-                            <label for="cmbSexo">Sexo</label>
-                            <select class="form-control input-md" id="cmbSexo" name="cmbSexo">
-                                <option value="M">Masculino</option>
-                                <option value="F">Feminino</option>
-                            </select>
-                        </div>
-                        
-                 
-                        
-                        
-                    </form>                    
-                    
-                    
-                </div>                
-            </div>
             
-        </div>
-        
-        
+            <!-- Linha para o formulario de cadastro -->
+            <div class="row">
+                <div class="col-md-12">
+                    <form name="frmManutUsuario" id="frmManutUsuario" method="post" action="manut_usuario.php" class="form">
+                        <fieldset class="panel panel-info">
+                            <div class="panel-heading">Dados Pessoais</div>
+
+                            <div class="panel-body">
+                                <div class="col-md-8 form-group">
+                                    <label for="txtNome">Nome</label>
+                                    <input type="text" name="txtNome" id="txtNome" class="form-control input-md" />
+                                </div>
+
+                                <div class="col-md-4 form-group">
+                                    <label for="txtDtNasc">Data de Nascimento</label>
+                                    <input type="date" name="txtDtNasc" id="txtDtNasc" class="form-control input-md"/>
+                                </div>
+
+                                <div class="col-md-4 form-group">
+                                    <label for="cmbEstado">Estado</label>
+                                    <select class="form-control input-md" id="cmbEstado" name="cmbEstado">
+                                        <option value="">-- Selecione --</option>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-4 form-group">
+                                    <label for="cmdCidade">Cidade</label>
+                                    <select class="form-control input-md" id="cmdCidade" name="cmdCidade">
+                                        <option value="">-- Selecione um Estado --</option>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-4 form-group">
+                                    <label for="cmbSexo">Sexo</label>
+                                    <select class="form-control input-md" id="cmbSexo" name="cmbSexo">
+                                        <option value="M">Masculino</option>
+                                        <option value="F">Feminino</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </fieldset> <!-- /fieldset dados pessoais -->
+
+                        <fieldset class="panel panel-info">
+                            <div class="panel-heading">Dados para Acesso</div>
+                            <div class="panel-body">
+                                <div class="col-md-6 form-group">
+                                    <label for="txtEmail">Email</label>
+                                    <input type="text" name="txtEmail" id="txtEmail" class="form-control input-md" />
+                                    <p class="help-block">Será enviada uma confirmação de cadastro para este email.</p>
+                                </div>
+
+                                <div class="col-md-6 form-group">
+                                    <label for="txtLogin">Login</label>
+                                    <input type="text" name="txtNome" id="txtLogin" class="form-control input-md" />
+                                </div>
+                            </div>
+                        </fieldset> <!-- /fieldset dados para acesso -->
+
+                        <!-- Controles do formulario -->
+                        <div class="form-inline pull-right">
+                            <button class="btn btn-default btn-md">Cancelar</button>
+                            <button class="btn btn-primary btn-md">Salvar</button>
+                        </div>
+
+                    </form>                    
+                </div> <!-- col que envolve o formulario inteiro -->
+            </div> <!-- /row (fim da linha para o formulario de cadastro) -->
+                 
+            
+        </div> <!-- /container -->
         
     </body>
     <script type="text/javascript" src="../libs/jquery-1.11.1.min.js" ></script>
