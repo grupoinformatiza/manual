df1fbd6ecbf47d6c80f1c8f5f48c49a63b98d0ec
Ana Paula <Ana Paula@ApSuaiden>
2014-08-10 19:02:41 -0300

    Alterações na entidade usuário para padronizar as entidades

---
+++ entidade/Tutorial.class.php
@@ -0,0 +1,61 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Entidade;
+use Exception;
+
+    class Tutorial extends Base{
+        private $nome;
+        private $tipo;
+        private $imagem;
+        
+        public function __construct($_codigo, $_nome, $_tipo, $_imagem) {
+            $this->codigo = $_codigo;
+            $this->nome = $_nome;
+            $this->tipo = $_tipo;
+            $this->imagem = $_imagem;
+        }
+        
+        public function setNome($_nome){
+            if ($nome <> "")
+                $this->nome = $_nome;
+            
+            $message = "Nome deve ser diferente de $_nome!";
+            throw new Exception($message);
+        }
+        
+        public function getNome(){
+            return $this->nome;
+        }
+        
+        public function setTipo($_tipo){
+            if ($_tipo <> "")
+                $this->tipo = $_tipo;
+            
+            $message = "tipo deve ser diferente de $_tipo!";
+            throw new Exception($message);
+        }
+        
+        public function getTipo(){
+            return $this->tipo;
+        }
+    
+        public function setImagem($_imagem){
+            if ($_imagem <> ""){
+                $this->imagem = $_imagem;
+            }
+            $message = "imagem deve ser diferente de $_imagem!";
+            throw new Exception($message);
+        }
+        
+        public function getImagem(){
+            return $this->imagem;
+        }
+        
+        }
+
+
