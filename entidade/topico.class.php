cd64944af0579bfec0c0e6f6826ab136c325e5d2
Ana Paula <Ana Paula@ApSuaiden>
2014-08-10 19:12:00 -0300

    correção de conflitos

--- entidade/topico.class.php
+++ entidade/topico.class.php
@@ -5,23 +5,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-    class topico extends base{
+namespace Entidade;
+use Exception;
+    class Topico extends Base{
         private $tipo;
         private $conteudo;
         private $tutorial;
         
         private function __construct($_codigo, $_tipo, $_conteudo, $_tutorial) {
-            $this->codigo = $_codigo;
-            $this->tipo = $_tipo;
-            $this->conteudo = $_conteudo;
-            $this->tutorial = $_tutorial;
+            $this->Codigo = $_codigo;
+            $this->Tipo = $_tipo;
+            $this->Conteudo = $_conteudo;
+            $this->Tutorial = $_tutorial;
         }
         
         
         public function setTipo($_tipo) {
-                if ($_tipo <> ""){
-                        $this->tipo = $_tipo;
-                }
+                if ($_tipo == "")
+                    throw new Exception("Nome deve ser preenchido");
+                $this->tipo = $_tipo;
+                
 
                 $message = "tipo deve ser diferente de $_tipo!";
                 throw new Exception($message);
@@ -32,9 +35,10 @@
         }
         
         public function setConteudo($_conteudo) {
-                if ($_conteudo <> ""){
-                        $this->conteudo = $_conteudo;
-                }
+                if ($_conteudo == "")
+                    throw new Exception(" deve ser preenchido");
+            $this->conteudo = $_conteudo;
+                
 
                 $message = "Conteudo deve ser diferente de $_conteudo!";
                 throw new Exception($message);
---
+++ ger/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home</title>
+        <link rel="stylesheet" href="../libs/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="layout/default.css">
+    </head>
+    <body role="document">
+        <?php require_once 'layout/cabecalho.php';?>
+        <div class="container">
+        
+            <div class="page-header">
+                <h1>Home</h1>
+            </div>
+            <h2> Olá, administrador</h2>
+            <div class="col-md-8 col-md-offset-2 imagem">
+                <img src="/imagens/equipe_informatiza.jpg" class="img-responsive img-rounded">
+            </div>
+        </div> 
+    </body>
+    <script type="text/javascript" src="../libs/jquery-1.11.1.min.js" ></script>
+    <script type="text/javascript" src="../libs/bootstrap/js/bootstrap.min.js"></script>
+</html>
