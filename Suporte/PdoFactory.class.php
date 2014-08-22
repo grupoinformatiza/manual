4b3d8f6e0db8cf47875c7ef40b92e9577bb82327
Fabio Yoshio <Fabio Yoshio@186.217.112.22>
2014-08-22 20:14:36 -0300

    Alterei a função valida campo em default.js

--- Suporte/PdoFactory.class.php
+++ Suporte/PdoFactory.class.php
@@ -7,7 +7,7 @@ class PdoFactory {
     
     
     public static function getConexao(){
-        $cn = new PDO("pgsql:host=127.0.0.1;dbname=manual", "postgres", "oklahoma1");
+        $cn = new PDO("pgsql:host=127.0.0.1;dbname=manual", "postgres", "sqladmin");
         $cn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $cn; 
     }
